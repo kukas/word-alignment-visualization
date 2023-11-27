d4f5bc697cebab2097174523f8679b4eaf99e775
Jirka Balhar <balhar.j@gmail.com>
2023-11-27 21:13:37 +0100

    fix long description

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='word-alignment-visualization',
     version='0.1',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
